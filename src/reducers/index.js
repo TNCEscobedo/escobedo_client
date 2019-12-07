6c43234e7f59a18d2f76d5a90eadc023e8b2279a
Escobedo Hackathon TNC <escobedotnc@gmail.com>
2019-12-07 17:48:52 -0600

    connected redux to main

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from "redux";
+import CobrosReducer from "./CobrosReducer";
+import ColoniasReducer from "./ColoniasReducer";
+import DescuentosReducer from "./DescuentosReducer";
+import InspectoresReducer from "./InspectoresReducer";
+import MercadosReducer from "./MercadosReducer";
+import PersonasReducer from "./PersonasReducer";
+import PuestosReducer from "./PuestosReducer";
+import TarifasReducer from "./TarifasReducer";
+import UsuariosReducer from "./UsuariosReducer";
+
+export default combineReducers({
+    cobros: CobrosReducer,
+    colonias: ColoniasReducer,
+    descuentos: DescuentosReducer,
+    inspectores: InspectoresReducer,
+    mercados: MercadosReducer,
+    personas: PersonasReducer,
+    puestos: PuestosReducer,
+    tarifas: TarifasReducer,
+    usuarios: UsuariosReducer
+})
